8687c6b5017e7548cfd6921c86ce53d27b098be4
anton <vao@asu-vei.ru>
2024-09-15 20:23:06 +0300

    [*] make install commands working properly

--- cmake/lzma.cmake
+++ cmake/lzma.cmake
@@ -1,6 +1,13 @@
 include(FetchContent)
 
 find_package(Git REQUIRED)
+
+set(XZ_DOC OFF)
+set(XZ_TOOL_LZMADEC OFF)
+set(XZ_TOOL_LZMAINFO OFF)
+set(XZ_TOOL_XZ OFF)
+set(XZ_TOOL_XZDEC OFF)
+
 FetchContent_Declare(LibLZMA
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/xz-utils.git
   GIT_TAG           master
