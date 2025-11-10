734d4a47d70bc363cfc88c4c9fa8998ae913bd65
anton <vao@asu-vei.ru>
2025-11-10 15:13:57 +0300

    [+] ZipUtil::DecompressFileToBa & getArchiveContentsList primary for the QXlsx

--- include/gen/files/ziputil.h
+++ include/gen/files/ziputil.h
@@ -15,7 +15,15 @@ public:
     static ZipUtil &getInstance();
     static Error::Msg CompressFile(const QString &filename, const QString &zipFileName);
     static Error::Msg CompressDir(const QString &dirname, const QString &zipFileName);
+    /// \brief Uncompress archive zipFileName contents to the directory dirname
     static Error::Msg DecompressFile(const QString &zipFileName, const QString &dirname);
+    /// \brief Uncompress file from zip archive
+    /// \param zipFileName - archive name
+    /// \param filename - filename from archive
+    /// \return QByteArray - the contents of filename if found, empty QByteArray if not
+    static QByteArray DecompressFileToBA(const QString &zipFileName, const QString &filename);
+    /// \brief Get archive zipFileName contents in QStringList with full paths
+    static QStringList getArchiveContentsList(const QString &zipFileName);
 
 private:
     static bool addFile(zip_t *za, const QString &filenameToAdd, const QString &zipFileName);
--- src/files/ziputil.cpp
+++ src/files/ziputil.cpp
@@ -143,6 +143,101 @@ Error::Msg ZipUtil::DecompressFile(const QString &zipFileName, const QString &di
     return Error::Msg::NoError;
 }
 
+QByteArray ZipUtil::DecompressFileToBA(const QString &zipFileName, const QString &filename)
+{
+    zip_t *za;
+    struct zip_file *zf;
+    struct zip_stat sb;
+    int err;
+    std::string filenamestr = filename.toStdString();
+    std::string zipfilenamestr = zipFileName.toStdString();
+    QByteArray ba;
+
+    if ((za = zip_open(zipfilenamestr.c_str(), 0, &err)) == nullptr)
+    {
+        zip_error_t error;
+        zip_error_init_with_code(&error, err);
+        qDebug() << "Cannot open zip archive " << zipFileName << ", error is: " << zip_error_strerror(&error);
+        zip_error_fini(&error);
+        return ba;
+    }
+
+    for (int i = 0; i < zip_get_num_entries(za, 0); i++)
+    {
+        if (zip_stat_index(za, i, 0, &sb) == 0)
+        {
+            if (strcmp(sb.name, filenamestr.c_str()) != 0)
+                continue;
+            zf = zip_fopen_index(za, i, 0);
+            if (!zf)
+            {
+                qDebug() << "Cannot open file in zip: " << sb.name;
+                return ba;
+            }
+
+            zip_uint64_t sum = 0;
+            QByteArray tmpba;
+            while (sum != sb.size)
+            {
+                zip_int64_t len = zip_fread(zf, tmpba.data(), 100);
+                if (len < 0)
+                {
+                    qDebug() << "Cannot read file in zip: " << sb.name;
+                    return ba;
+                }
+                ba += tmpba;
+                sum += len;
+            }
+            zip_fclose(zf);
+        }
+        else
+            qFatal("Zip_stat_index error");
+    }
+
+    if ((zip_close(za)) < 0)
+    {
+        qDebug() << "Cannot close archive " << zipFileName << ", error: " << zip_strerror(za);
+        zip_discard(za);
+        return ba;
+    }
+    return ba;
+}
+
+QStringList ZipUtil::getArchiveContentsList(const QString &zipFileName)
+{
+    zip_t *za;
+    struct zip_stat sb;
+    int err;
+    std::string zipfilenamestr = zipFileName.toStdString();
+    QStringList sl;
+
+    if ((za = zip_open(zipfilenamestr.c_str(), 0, &err)) == nullptr)
+    {
+        zip_error_t error;
+        zip_error_init_with_code(&error, err);
+        qDebug() << "Cannot open zip archive " << zipFileName << ", error is: " << zip_error_strerror(&error);
+        zip_error_fini(&error);
+        return sl;
+    }
+
+    for (int i = 0; i < zip_get_num_entries(za, 0); i++)
+    {
+        if (zip_stat_index(za, i, 0, &sb) == 0)
+        {
+            sl << sb.name;
+        }
+        else
+            qFatal("Zip_stat_index error");
+    }
+    if ((zip_close(za)) < 0)
+    {
+        qDebug() << "Cannot close archive " << zipFileName << ", error: " << zip_strerror(za);
+        zip_discard(za);
+        return sl;
+    }
+    return sl;
+}
+
 bool ZipUtil::addFile(zip_t *za, const QString &filenameToAdd, const QString &zipFileName)
 {
     zip_source_t *sourceFile;
