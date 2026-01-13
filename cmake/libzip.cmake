7c9c2fea114422e9728f9c1bfa1777e2195f9308
anton <vao@asu-vei.ru>
2026-01-13 11:05:51 +0300

    [^] libzip

--- cmake/libzip.cmake
+++ cmake/libzip.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(libzip
   GIT_REPOSITORY    https://git.avmenergo.ru/avm-energo/libzip.git
-  GIT_TAG           v1.11.4
+  GIT_TAG           v1.11.5
 )
 
 FetchContent_MakeAvailable(libzip)
