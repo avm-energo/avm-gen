016ae86c0eaa6e957b91b2372a5e6107a84cb222
anton <vao@asu-vei.ru>
2026-04-09 14:06:22 +0300

    Update libzip.cmake

--- cmake/libzip.cmake
+++ cmake/libzip.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 
 FetchContent_Declare(libzip
-  GIT_REPOSITORY    git@github.com:avm-energo/libzip.git
+  GIT_REPOSITORY    https://github.com/avm-energo/libzip.git
   GIT_TAG           v1.11.5
 )
 
